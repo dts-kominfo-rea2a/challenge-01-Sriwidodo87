08b21bcbb985a4c8fd1b7bb739ec47824e55b53c
SriWidodo <swidodo87@gmail.com>
2022-05-29 20:39:40 +0800

    fead:Tugas Struktur Data JS

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+// original code
+//const firstUser = {};
+//const secondUser = {};
+
+const firstUser ={
+id :1,
+nama :"Monica",//string
+gender :"Female",//boolean false
+age :17,//number
+email:"monica@dingdong.com",//string
+favoriteColor : ['Yellow', 'Pink', 'White', 'Purple' ],//array
+isHavePet:'Yes',//string
+education :[
+    {name:"SD01",  city: "Jakarta",  graduate: 2016},
+    {name:"SMP02", city: "Jakarta", graduate: 2019 },
+    {name:"SMA03", city:"Tangerang",graduate:null },
+],//array
+favoriteRestaurant:[
+'Bento', 'Sushi', "Pancake", "Eggy", "Tempura",  "Padang", "Tteok", "Sushi"
+]//array
+};
+const secondUser = {
+    id:2,
+nama :"Wendy",//string
+gender:"Male",// boolean true(male)
+age :23,//number
+email:"wendy@dingdong.com",//string
+favoriteColor : [ 'Blue', 'Black', 'Grey'],//array
+isHavePet:'no',//string
+education :[
+    {name: "SD 02", city: "Jakarta", graduate: 2010 },
+    {name:  "SMP 03", city: "Bogor", graduate: 2013 },
+    {name: "SMA 01", city: "Surabaya", graduate: 2016 },
+],//array
+
+favoriteRestaurant:[
+    'Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek','Eggy'
+    ]//array
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+//original code
+//const users = [];
+const users=[firstUser,secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
